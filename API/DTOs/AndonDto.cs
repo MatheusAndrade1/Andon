4cd000b629e798d0ad8fe4d4d1224c95de830827
MatheusAndrade1 <matheussantana1997@hotmail.com>
2022-06-08 10:00:01 -0300

    Updated the AndonDto to also return the id

--- API/DTOs/AndonDto.cs
+++ API/DTOs/AndonDto.cs
@@ -2,6 +2,7 @@ namespace API.DTOs
 {
     public class AndonDto
     {
+        public int id { get; set; }
         public string type { get; set; }
         public int warnCount { get; set; }
         public int alarmCount { get; set; }
