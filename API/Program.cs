2875afad9e30f39bed4ab862ed053dd4c941bbe3
MatheusAndrade1 <matheussantana1997@hotmail.com>
2022-06-22 16:54:49 -0300

    Resolved warning in Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -7,7 +7,7 @@ namespace API
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
